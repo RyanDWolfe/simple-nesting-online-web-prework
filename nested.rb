bac0de597a16eca4528a0189e0c1fa78ab277097
Ryan Wolfe <ryan@studiohopfitness.com>
2019-01-31 18:32:08 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -20,7 +20,7 @@ hopper = programmer_hash[:grace_hopper]
 end
 
 def alan_kay_is_known_for
-	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
+	hopper = programmer_hash[:alan_key][:known_for]
 
 	programmer_hash =
  		{
