278c8fd320594d0d8e18927d450154c06af0e343
Ryan Wolfe <ryan@studiohopfitness.com>
2019-01-31 18:36:08 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -55,7 +55,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
-		 hopper = programmer_hash[:dennis_ritchie][:languages]
+		 hopper = programmer_hash[:dennis_ritchie][:languages].to_s
 
 end
 
