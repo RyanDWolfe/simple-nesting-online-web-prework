edef1ea3c540bc52e640558dab34ef97ba0b4c36
Ryan Wolfe <ryan@studiohopfitness.com>
2019-01-31 18:34:30 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -36,7 +36,7 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
-		 hopper = programmer_hash[:alan_key][:known_for]
+		 hopper = programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
